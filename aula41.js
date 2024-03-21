fdf294124bdd8ac5e1ee183913208d5d7617abf3
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2024-03-21 16:48:30 -0300

    treinando sozinho a remover elementos do DOM  em JS

--- aula41.js
+++ aula41.js
@@ -6,17 +6,35 @@ curso.map((el,chave)=>{
     const newElement = document.createElement("div")
     newElement.setAttribute("id","c"+chave)
     newElement.setAttribute("class","curso c1")
-    newElement.innerHTML=el
-    newElement.style.backgroundColor = "lightgreen"
-
-    const lixeira = document.createElement("img")
-    lixeira.setAttribute("src","./icone-lixo.png")
-    lixeira.setAttribute("class","lixeira")
-    
-    lixeira.addEventListener("click",(evt)=>{
-        caixa.removeChild(evt.target.parentNode)//removendo o elemento (evt) e o pai deste elemento que foi clicado
-    })
-
-    newElement.appendChild(lixeira)
+    newElement.innerHTML = el
+    newElement.style.backgroundColor = "#ddff"
     caixa.appendChild(newElement)
+
+    const lixo = document.createElement("img")
+    lixo.setAttribute("src","./icone-lixo.png")
+    lixo.setAttribute("class","lixeira")
+    newElement.appendChild(lixo)
+
+    lixo.addEventListener("click",(evt)=>{
+        caixa.removeChild(evt.target.parentNode)
+    })
 })
+
+// curso.map((el,chave)=>{
+//     const newElement = document.createElement("div")
+//     newElement.setAttribute("id","c"+chave)
+//     newElement.setAttribute("class","curso c1")
+//     newElement.innerHTML=el
+//     newElement.style.backgroundColor = "lightgreen"
+
+//     const lixeira = document.createElement("img")
+//     lixeira.setAttribute("src","./icone-lixo.png")
+//     lixeira.setAttribute("class","lixeira")
+    
+//     lixeira.addEventListener("click",(evt)=>{
+//         caixa.removeChild(evt.target.parentNode)//removendo o elemento (evt) e o pai deste elemento que foi clicado
+//     })
+
+//     newElement.appendChild(lixeira)
+//     caixa.appendChild(newElement)
+// })
