ff4a1e288fb1a6978e50713a984def5cc61a0146
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2024-04-16 15:58:13 -0300

    removendo elemento selecionado

--- aula43.js
+++ aula43.js
@@ -5,6 +5,7 @@ const btn_c = [...document.querySelectorAll(".curso")]
 const c1_2 = document.querySelector("#c1_2")
 const cursos = ["HTML","CSS","Javascript","PHP","React","MySQL","React Native"]//function map()
 const btnSelection = document.getElementById("btnSelection")
+const btnRemoved = document.getElementById("btnRemoved")
 
 cursos.map((el,chave)=>{
     //criando e dando atributos em um novo elemento (div) só no JS
@@ -20,40 +21,52 @@ cursos.map((el,chave)=>{
     const rb = document.createElement("input")
     rb.setAttribute("type","radio")
     rb.setAttribute("name","rb_curso")
-
+    
     //colocando os elementos em funcionamento devidamente ao ser pai (parent)
     comandos.appendChild(rb)
     newElement.appendChild(comandos)
     caixaCursos.appendChild(newElement)
+})
 
-    const cursoSelection = () =>{
+    const radioSelection = () =>{
         //criando um input com atributo
         const radio = [...document.querySelectorAll("input[type=radio]")]
     
         //criando uma variavel que filtra o elmento anterior e retorna o elemento checkado
-        const radiousSelection = radio.filter((el)=>{
+        const radioSelection = radio.filter((el)=>{
             return el.checked
         })
     
         //a variavel recebe ela na primeira posição
-        return radiousSelection[0]
+        return radioSelection[0]
     }
 
     //criando um evento de click no elemento (btnSelection)
     btnSelection.addEventListener("click",(evt)=>{
 
-        //criando um elemento que recebe a posição da variavel < o pai dela < avô < primeiro elemento filho < e o texto contido dentro desse elemento
-        const cursoSelection = radiousSelection.parentNode.parentNode.firstChild.textContent
-
-        alert("Curso selecionado: " +  cursoSelection)
-
-        //monstrando no console os resultados
-
-        console.log(radio)
-        console.log(radiousSelection)
-        console.log(cursoSelection)
+        const rs =radioSelection()
+        try{//try == if
+            //criando um elemento que recebe a posição da variavel < o pai dela < avô < primeiro elemento filho < e o texto contido dentro desse elemento
+            const cursoSelection = rs.parentNode.parentNode.firstChild.textContent
+            alert("Curso selecionado: " + cursoSelection)
+        }catch(ex){
+            alert("Selecione um curso")
+        }
     })
-})
+
+    btnRemoved.addEventListener("click",(evt)=>{
+        const rs = radioSelection()
+        if(rs!=undefined){
+            const cursoSelection = rs.parentNode.parentNode
+            cursoSelection.remove()
+        }else{
+            alert("Selecione um curso")
+        }
+    })
+
+    console.log(radio)
+    console.log(radiousSelection)
+    console.log(cursoSelection)
 
 // const caixaCursos = document.querySelector("#caixaCursos")
 // const btn_c = [...document.querySelectorAll(".curso")]
