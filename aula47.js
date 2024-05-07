f7397d642c9cfda55de07529a6cf03ee14daf25e
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2024-05-07 17:27:42 -0300

    Aprendendo sobre o Método FIND e começando a manipular  no JS

---
+++ aula47.js
@@ -0,0 +1,7 @@
+const p_array = document.querySelector("#array")
+const txt_search = document.querySelector("#txt_search")
+const btnSearch = document.querySelector("#btnSearch")
+const result = document.querySelector("#result")
+
+//montando um array numérico
+const element_array = [10,5,8,2,9,15,20]
