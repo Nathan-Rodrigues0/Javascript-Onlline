fc9ba60faefac5a5bcfb48a60f31e5d53788ffea
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2024-05-09 15:19:44 -0300

    aula 47 finalizada
    Treinando com método FIND

--- aula47.js
+++ aula47.js
@@ -4,4 +4,16 @@ const btnSearch = document.querySelector("#btnSearch")
 const result = document.querySelector("#result")
 
 //montando um array numérico
-const element_array = [10,5,8,2,9,15,20]
+const element_array = ["html", "css", "Javascript", "c++", "python"]
+p_array.innerHTML = "[" + element_array + "]"
+
+btnSearch.addEventListener("click",(evt)=>{
+    result.innerHTML = "Valor não encontrado"
+    const ret = element_array.find((el,i)=>{
+        if(el.toUpperCase() == txt_search.value.toUpperCase()){
+            result.innerHTML = "Valor encontrado: " + el + " na posição: " + i
+            return el
+        }
+    })
+    console.log(ret)
+})
