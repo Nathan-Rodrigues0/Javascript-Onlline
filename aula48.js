4b576186a6f659acaa059798983829b6a270109b
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2024-05-15 16:20:49 -0300

    comentando o passo a passo do método EVERY

--- aula48.js
+++ aula48.js
@@ -1,11 +1,15 @@
+//criando constantes
 const p_array = document.querySelector("#array")
 const btnVerify = document.querySelector("#btnVerify")
 const result = document.querySelector("#result")
 
+//criando array
 const element_array = [42,24,53,43,35,89]
 p_array.innerHTML = "[" + element_array + "]"
 
+//criando um evento de click
 btnVerify.addEventListener("click",(evt)=>{
+    //criando um elemento que verifica todos os elemento do array
     const ret = element_array.every((e,i)=>{
         if(e<18){
             result.innerHTML = "Array não conforme na posição " + i
