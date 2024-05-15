16ceb827c06a20be2ba7f043c333df5e20b0743c
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2024-05-15 16:37:10 -0300

    refazendo o exercício

--- aula48.js
+++ aula48.js
@@ -1,22 +1,41 @@
-//criando constantes
-const p_array = document.querySelector("#array")
-const btnVerify = document.querySelector("#btnVerify")
+const array = document.querySelector("#array")
+const verify = document.querySelector("#btnVerify")
 const result = document.querySelector("#result")
 
-//criando array
-const element_array = [42,24,53,43,35,89]
-p_array.innerHTML = "[" + element_array + "]"
+const el_array = [32,42,64,45,21,75]
+array.innerHTML = "[" + el_array + "]"
 
-//criando um evento de click
-btnVerify.addEventListener("click",(evt)=>{
-    //criando um elemento que verifica todos os elemento do array
-    const ret = element_array.every((e,i)=>{
+verify.addEventListener("click",(evt)=>{
+    const res = el_array.every((e,i)=>{
         if(e<18){
             result.innerHTML = "Array não conforme na posição " + i
         }
-        return e>=18
+        return e >= 18
     })
-    if(ret){
+    if(res){
         result.innerHTML = "OK"
     }
 })
+
+// //criando constantes
+// const p_array = document.querySelector("#array")
+// const btnVerify = document.querySelector("#btnVerify")
+// const result = document.querySelector("#result")
+
+// //criando array
+// const element_array = [42,24,53,43,35,89]
+// p_array.innerHTML = "[" + element_array + "]"
+
+// //criando um evento de click
+// btnVerify.addEventListener("click",(evt)=>{
+//     //criando um elemento que verifica todos os elemento do array
+//     const ret = element_array.every((e,i)=>{
+//         if(e<18){
+//             result.innerHTML = "Array não conforme na posição " + i
+//         }
+//         return e>=18
+//     })
+//     if(ret){
+//         result.innerHTML = "OK"
+//     }
+// })
