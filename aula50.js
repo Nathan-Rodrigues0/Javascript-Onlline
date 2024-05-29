56964dc46b85d815c34d19fd950237092ff38469
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2024-05-29 17:07:02 -0300

    refazendo o exercícios e comentando passo a passo

--- aula50.js
+++ aula50.js
@@ -1,18 +1,38 @@
+//criando constantes para os tres elementos principais da página
 const array = document.querySelector("#array")
-const btnReduce = document.querySelector("#btnReduce")
+const reduce = document.querySelector("#btnReduce")
 const result = document.querySelector("#result")
 
+//criando um elemento que recebe o array
 const el_array = [1,2,3,4,5]
-let ant = []
-let atu = []
-
 array.innerHTML = "[" + el_array + "]"
+let ant = []
 
-btnReduce.addEventListener("click",(evt)=>{
+//criando um evento ao clicar no botão reduce
+reduce.addEventListener("click",(evt)=>{
+    //escrevendo uma função com o método REDUCE
     result.innerHTML = el_array.reduce((previous,current,position)=>{
         ant.push(previous)
-        atu.push(current)
+        //retornando o valor atual + valor anterior
         return current + previous
     })
-    result.innerHTML+="<br/> Valor anterior: "+ant+"<br/> Vallor atual: "+atu
 })
+
+// const array = document.querySelector("#array")
+// const btnReduce = document.querySelector("#btnReduce")
+// const result = document.querySelector("#result")
+
+// const el_array = [1,2,3,4,5]
+// let ant = []
+// let atu = []
+
+// array.innerHTML = "[" + el_array + "]"
+
+// btnReduce.addEventListener("click",(evt)=>{
+//     result.innerHTML = el_array.reduce((previous,current,position)=>{
+//         ant.push(previous)
+//         atu.push(current)
+//         return current + previous
+//     })
+//     result.innerHTML+="<br/> Valor anterior: "+ant+"<br/> Vallor atual: "+atu
+// })
