b6887d68fd46563596c32fc1f826721f2ccb92d1
Nathan de Paula Rodrigues <114612552+Nathan-Rodrigues0@users.noreply.github.com>
2024-06-25 14:51:39 -0300

    Funcionalidade do Array

--- aula52.js
+++ aula52.js
@@ -1,9 +1,21 @@
 const box = document.querySelector("#box")
 
-let curso = ["HTML","Javascript","CSS"]
+let cursos = ["HTML","Javascript","CSS"] // um [array] com strings
 
-curso.map((el)=>{
-    let p = document.createElement("p")
+cursos[1]="C++" // alterando a segunda posição do array [cursos]
+
+cursos.unshift("Python") // adiciona o elemento no ínicio do array [cursos]
+
+cursos.shift() // remove o primeiro elemento do array [cursos]
+
+//*cursos.push("Python") // adicionando elemento no final do array [cursos]
+
+//*cursos.pop() // remove o último elemento do array [cursos]
+
+console.log(cursos[1])//[] = posição do elemento dentro do array
+
+cursos.map((el)=>{//percorrendo o elemento curso e dando uma funcionalidade
+    let p = document.createElement("p")//criando um parágrafo
     p.innerHTML = el
-    box.appendChild(p)
+    box.appendChild(p)//adicionando o conteúdo do [curso] para a constante #box
 })
